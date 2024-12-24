4ebd805ef882d88b6184b3df54c4d3b38d4d4a92
Jason2Car <91041525+Jason2Car@users.noreply.github.com>
2024-12-24 17:30:10 -0600

    Update LDMatrixCode.py
    Made it such that response data is properly in taken

--- LDMatrixCode.py
+++ LDMatrixCode.py
@@ -6,39 +6,53 @@ import math
 from keras import layers
 from matplotlib import pyplot
 
-in_train = open('LDMatrix_train.txt', 'r').read()
-in_test = open('LDMatrix_test.txt', 'r').read()
-response_train = open('response_train.txt', 'r').read()
-response_test = open('response_test.txt', 'r').read()
+train_data = open('LDMatrix_train.txt', 'r').read()
+test_data = open('LDMatrix_test.txt', 'r').read()
+train_response = open('train_response.txt', 'r').read()
+test_response = open('test_response.txt', 'r').read()
 
 #If you want to change this to manually setting, go ahead
 input = "test	1	1 1	1	1	relu			10	32"
 
-#Seperate into each case, 1092 cases in total
-in_train = in_train.split("\n") #inputs
-in_test = in_test.split("\n") #expected response_trains
-#Remove the SNP identifiers and ends are blank
-in_train = in_train[1:8300]
-in_test = in_test[1:8300]
+train_data = train_data.split("\n") #inputs
+test_data = test_data.split("\n") 
+train_response = train_response.split("\n") #expected outputs
+test_response = test_response.split("\n") 
+
+#Remove the begining SNP identifiers and ending are blanks
+train_data = train_data[1:8300]
+test_data = test_data[1:8300]
+test_response = in_train[1:874]
+test_response = in_test[1:220]
 
 #Creating arrays to hold data
-train = []
-test = []
-for i in in_train:
+train_in = []
+test_in = []
+train_out = []
+test_out = []
+
+for i in train_data:
     cur = (i.split())#Create an array to hold data in the case
     cur = cur[1:] #Remove the "case #" at the start of every case
     for i in range(len(cur)):#Seperate the data in that case
         cur[i] = float(cur[i])#Convert the String into numbers
-    train.append(np.array(cur))#Add the array to the data set
+    train_in.append(np.array(cur))#Add the array to the data set
 
 #Repeat process with the test data
-for i in in_test:
+for i in test_data:
     cur = (i.split())
     cur = cur[1:]
     for i in range(len(cur)):
         cur[i] = float(cur[i])
-    test.append(np.array(cur))
+    test_in.append(np.array(cur))
 
+for i in train_response:
+    cur = i.split()
+    train_out.append(float(cur[1]))
+    
+for i in test_response:
+    cur = i.split()
+    test_out.append(float(cur[1]))
 # #Seperate the data into testing and training data
 # train_data = np.array(train[:math.floor(len(train)*0.8)])
 # train_targets= np.array(test[:math.floor(len(test)*0.8)])
@@ -113,36 +127,37 @@ loss = "mse"
 
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-model_cnn1 = keras.Sequential()
+model_cnn = keras.Sequential()
 #print(train_data.shape)
 #Plug in the number of filters, size of filters, activation functions, and the 
-model_cnn1.add(Conv2D(filters = numFilters[0], kernel_size = (sizeFilters[0], sizeFilters[0]), activation=cActivation, input_shape=(8299, 8299,1)))
+model_cnn.add(Conv2D(filters = numFilters[0], kernel_size = (sizeFilters[0], sizeFilters[0]), activation=cActivation, input_shape=(8299, 8299,1)))
 
 for i in range(cLayers):
-    model_cnn1.add(layers.Conv2D(filters = numFilters[i], kernel_size = (sizeFilters[i], sizeFilters[i]), activation = cActivation))
+    model_cnn.add(layers.Conv2D(filters = numFilters[i], kernel_size = (sizeFilters[i], sizeFilters[i]), activation = cActivation))
 
 #should be no need pooling layers, can test if want to later
 
 # Flatten the output of the Conv1D layer
-model_cnn1.add(layers.Flatten())
+model_cnn.add(layers.Flatten())
 for i in range(nLayers):
-    model_cnn1.add(layers.Dense(density[i], activation = nActivation))
+    model_cnn.add(layers.Dense(density[i], activation = nActivation))
 
 #model_cnn.add(layers.Dense(1, activation = activation)) # not sure if we want the final to have a activation
 
-model_cnn1.add(layers.Dense(8299))
+#requires some clarification on size of output
+model_cnn.add(layers.Dense(len(train_out)))
 
 # Summary of your model
-model_cnn1.summary()
+model_cnn.summary()
 
 # Model compilation
-model_cnn1.compile(optimizer = optimizer, loss = loss)
+model_cnn.compile(optimizer = optimizer, loss = loss)
 
 
 #May need to change or manually set epochs/batch size as needed
-model_cnn1.fit(in_train, response_train, batch_size = batch_size, epochs = epochs)
+model_cnn.fit(train_data, train_response, batch_size = batch_size, epochs = epochs)
 
 # Model Evaluation
-evaluate_test = model_cnn1.evaluate(in_test, response_test, verbose = 0)
+evaluate_test = model_cnn.evaluate(test_data, test_response, verbose = 0)
 
 print('Test loss', evaluate_test)
